af757ef89ed7ffb4b331f4bd3c334f473a2d5f70
gabrielmagno <gabrielmagno1@gmail.com>
2013-07-31 14:38:32 -0300

    Added status message when converting or creating table of ids

--- tools/calculate.sh
+++ tools/calculate.sh
@@ -5,11 +5,13 @@ file_out=$2
 
 if [ ! -f $file_base.graph ];
 then
+    echo "* Converting graph"
     ./tools/convert.sh $file_base
 fi
 
 if [ ! -f $file_base.tableids ];
 then
+    echo "* Creating table of ids"
     awk '{if (NR > 2) {print $1, $1}}' $file_base.graph | sort -k 1n > $file_base.tableids
 fi
 
